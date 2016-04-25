e4528a90d62a0e044e4b50e019a3000b3c9f44d6
SamuelFinocchioNJP <samuelfinocchioit@gmail.com>
2016-04-25 08:32:34 +0200

    Server test

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-/**
- * Created by informaticage on 24/04/16.
- */
-console.log("Node test");
+var http = require('http');
+function onRequest(request, response){
+    console.log("A user made a request" + request.url);
+    response.writeHead(200, {"Context-Type": "text/plain"});
+    response.write("This is my response");
+    response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server now running..");
