fb0b59455256058d05f1c32717594011c946dda3
SamuelFinocchioNJP <samuelfinocchioit@gmail.com>
2016-06-28 15:46:42 +0200

    Update elearningDB.sql

--- elearningDB.sql
+++ elearningDB.sql
@@ -42,11 +42,58 @@ CREATE TABLE IF NOT EXISTS Own (
     CONSTRAINT UNIQUE (username, id_course)
 );
 
-// Registration
-INSERT INTO User (username, name, surname, email, password) VALUES ('InformaticageNJP', 'Samuel', 'Finocchio', 'samuelfinocchioit@gmail.com', 'b7a875fc1ea228b9061041b7cec4bd3c52ab3ce3');
+-- Registration
+INSERT INTO User (username, name, surname, email, password)
+    VALUES ('InformaticageNJP', 'Samuel', 'Finocchio', 'samuelfinocchioit@gmail.com', 'b7a875fc1ea228b9061041b7cec4bd3c52ab3ce3');
 
-// Getting content of a course
-SELECT Content.id_content,Content.title, Content.url, Content.type  FROM Content INNER JOIN Has ON Content.id_content = Has.id_content INNER JOIN Course ON Course.id_course = Has.id_course AND Course.id_course = 1;
+-- Getting content of a course
+SELECT Content.id_content,Content.title, Content.url, Content.type 
+        FROM Content 
+            INNER JOIN Has ON Content.id_content = Has.id_content 
+            INNER JOIN Course ON Course.id_course = Has.id_course 
+            WHERE Course.id_course = $id_course;
 
-// Getting content of a course with a specified content type
-SELECT Content.id_content,Content.title, Content.url, Content.type  FROM Content INNER JOIN Has ON Content.id_content = Has.id_content INNER JOIN Course ON Course.id_course = Has.id_course AND Course.id_course = 1 AND Content.type = 'video';
+-- Getting content of a course with a specified content type
+SELECT Content.id_content,Content.title, Content.url, Content.type
+    FROM Content
+    INNER JOIN Has ON Content.id_content = Has.id_content
+    INNER JOIN Course ON Course.id_course = Has.id_course
+    AND Course.id_course = 1
+    AND Content.type = 'video';
+
+-- Login with user informations
+SELECT username, name, surname, email, password
+    FROM User
+    WHERE username = '$username' AND password = '$password';
+                    
+-- Set viewed
+INSERT INTO Attended (username, id_content)
+    VALUES ('$username', $id_content);
+    
+-- Set owner
+INSERT INTO Own (username, id_course) 
+    VALUES ('$username', $id_owner);
+    
+-- Add course
+INSERT INTO Course (username, title, description, difficulty) 
+    VALUES ('$username', '$title', '$description', '$difficulty')
+    
+-- Get courses
+SELECT id_course, title, description, difficulty, username
+    FROM Course;
+                
+-- Get user courses
+SELECT id_course, title, description, difficulty, username FROM Course
+    WHERE username = '$username';
+    
+-- Delete content
+DELETE FROM Content
+    WHERE id_content = $id_content;
+    
+-- Create content
+INSERT INTO Content (title, url, type)
+    VALUES ('$title', '$url', '$type');
+    
+-- Relates content
+INSERT INTO Has (id_content, id_course)
+    VALUES ($id_content, $id_course);
